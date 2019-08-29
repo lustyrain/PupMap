ad34449a3d51b078c242ea3bc9c29396d2ebbf1d
LustyRain <rastyrein.ken.091012@gmail.com>
2019-08-29 15:01:40 +0900

    fis setter

--- PupMap.js
+++ PupMap.js
@@ -1,42 +1,64 @@
 const puppeteer = require('puppeteer');
 
 class PupMap {
+  set emulateOptions(value) {
+    this._emulateOptions = value;
+  }
+  set options(value) {
+    this._options = value;
+  }
+  set setBeforeInitFunction(func) {
+    this._beforeInitFunction = func;
+  }
+  set setAfterInitFunction(func) {
+    this._afterInitFunction = func;
+  }
+  set setBeforeGotoFunction(func) {
+    this._beforeGotoFunction = func;
+  }
+  set setAfterGotoFunction(func) {
+    this._afterGotoFunction = func;
+  }
+  set setBeforeCloseFunction(func) {
+    this._beforeCloseFunction = func;
+  }
+  set setAfterCloseFunction(func) {
+    this._afterCloseFunction = func;
+  }
   constructor() {
     this.browser = false;
     this.page = false;
-    this.options = {};
-    this.emulateOptions = {};
-    this.beforeInitFunction = function() {};
-    this.afterInitFunction = function() {};
-    this.beforeCloseFunction = function() {};
-    this.afterCloseFunction = function() {};
+    this._options = {};
+    this._emulateOptions = {};
+    this._beforeInitFunction = function() {};
+    this._afterInitFunction = function() {};
+    this._beforeGotoFunction = function() {};
+    this._afterGotoFunction = function() {};
+    this._beforeCloseFunction = function() {};
+    this._afterCloseFunction = function() {};
   }
 
   async init() {
-    this.beforeInitFunction();
-    this.browser = await puppeteer.launch(this.options);
+    this._beforeInitFunction();
+    this.browser = await puppeteer.launch(this._options);
     this.page = await this.browser.newPage();
-    await this.page.emulate(this.emulateOptions);
-    this.afterInitFunction();
+    await this.page.emulate(this._emulateOptions);
+    this._afterInitFunction();
+  }
+
+  async goto(url) {
+    this._beforeGotoFunction();
+    const data = await this.page.goto(url);
+    this._afterGotoFunction();
+    return data;
   }
 
   async close() {
-    this.beforeCloseFunction();
+    this._beforeCloseFunction();
     this.browser.close();
-    this.afterCloseFunction();
+    this._afterCloseFunction();
   }
 
-  setBeforeInitFunction(func) {
-    this.beforeInitFunction = func;
-  }
-  setAfterInitFunction(func) {
-    this.afterInitFunction = func;
-  }
-  setBeforeCloseFunction(func) {
-    this.beforeCloseFunction = func;
-  }
-  setAfterCloseFunction(func) {
-    this.afterCloseFunction = func;
-  }
+
 }
 module.exports = PupMap;
